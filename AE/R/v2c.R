37465c66bdaffbcf6bfe7bb20d19c71a63dde1ef
Henrik Karlsson <henkar91@gmail.com>
2017-09-16 12:06:41 +0200

    working package

--- AE/R/v2c.R
+++ AE/R/v2c.R
@@ -2,10 +2,11 @@
 #' @name v2c
 #' @param data A data frame.
 #' @param id name of id variable, within "".
-#' @param relevance_root_q relevance question root, separated with _ followed by brand_id, within "".
-#' @param closeness_root_q relevance question root, separated with _ followed by brand_id, within "".
-#' @return Returns transposed data frame.
-#' @description Function works similar like SPSSs VarsToCases. The function selects id, all relevance and closeness questions and transpose them only. Purpose of function is to prepare data for AE function.
+#' @param relevance_root_q question root for relevance variables, format questionroot_brandid, within "".
+#' @param closeness_root_q question root for closeness variables, format questionroot_brandid, within "".
+#' @return Returns transposed data frame (long format).
+#' @description Function is intended to work as SPSSs VarsToCases
+
 
 v2c <-
 function(data, id, relevance_root_q, closeness_root_q){
--- v2c.R
+++ v2c.R
@@ -1,28 +1,19 @@
 
-v2c <- function(data, id, relevance_root_q, closeness_root_q){
-    stopifnot(is.character(id),
-              is.character(relevance_root_q),
-              is.character(closeness_root_q))
-    
-    rel <- data %>%
-        select(id, starts_with(relevance_root_q)) %>%
-        gather(variable, value, -respid, na.rm = TRUE) %>%
-        separate(variable, into = c("question", "brand_id"), 
-                 sep = "_", remove = T) %>% 
-        select(-question, relevance = value)
-    
-    
-    clo <- data %>%
-        select(id, starts_with(closeness_root_q)) %>%
-        gather(variable, value, -respid, na.rm = TRUE) %>%
-        separate(variable, into = c("question", "brand_id"), 
-                 sep = "_", remove = T) %>% 
-        select(-question, closeness = value)
-    
-    
-    ae <- left_join(rel, clo, by = c(id, "brand_id"))
-    
-    return(ae)
-}
-
-# v2c(df, "respid", "q1", "q2")
+ae <-
+    function(data, id, relevance, closeness){
+        stopifnot(is.data.frame(data),
+                  is.numeric(relevance),
+                  is.numeric(closeness))
+        
+        data <- data %>%
+            select(id2 = id, relevance, closeness) %>%
+            mutate(avg_brand_score = (relevance + closeness)/2) %>%   # Compute average brand score
+            group_by(id2) %>%                                         # Group data per respondent
+            mutate(rank = rank(-avg_brand_score),                     # Rank avg brand score for each respondent
+                   step1 = 1/rank^1.35,                               # Compute numerator
+                   step2 = sum(step1),                                # Compute denominator  
+                   ae = step1/step2 * 100) %>%
+            select(id = id2, relevance, closeness, avg_brand_score, ae)
+        
+        return(data)
+    }
