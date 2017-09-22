7606230f5c1d60df3a14bf607cdadb39e2837f36
Henrik Karlsson <henkar91@gmail.com>
2017-09-22 09:25:58 +0200

    Updated reverse scale

--- nordicmds/R/reverse_scale.R
+++ nordicmds/R/reverse_scale.R
@@ -1,10 +1,16 @@
 #' @title Reverse Scale
 #' @name reverse_scale
-#' @param x 
-#' @return x
-#' @description x 
+#' @param high The maximum expected value of the scale  
+#' @return Returns reversed input
+#' @description function to reverse numbers in a discrete scale, given input of highest expected value. 
 #' @export
+#' @examples
+#' # Reverse vector as if it were a scale of 1 through 7
+#' reverse_scale(c(1,2,3,5,4,NA), high = 7)
 
-reverse_scale = function(x) {
-    (max(x, na.rm=T) + 1) - x
+reverse_scale = function(x, high) {
+    stopifnot(is.numeric(x))
+    if(max(x, na.rm = TRUE) > high) 
+        stop("Error: There are values larger than 'high'")
+    high + 1 - x
 }
